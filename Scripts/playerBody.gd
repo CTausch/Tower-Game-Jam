fe6b0b2a4985b58f86455516d7393a16935e6d81
JaggerV20 <cenobia620@gmail.com>
2024-09-18 11:53:12 -0500

    Created basic movement and lines
    Lines that player and enemy will follow have been created. The LineBase script initializes all the lines. As of now, inputting up or down just prints the current position. Nothing is visible, and I don't know why. It does seem to be working despite this.

---
+++ Scripts/playerBody.gd
@@ -0,0 +1,20 @@
+extends Area2D
+
+@export var lineBaseReference : Node2D
+var currentLine
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	currentLine = lineBaseReference.defaultLine
+
+func _physics_process(_delta: float) -> void:
+	if Input.is_action_just_pressed("ui_up") and currentLine.above != lineBaseReference.upperBound:
+		currentLine = currentLine.above
+		position = currentLine.points[1]
+		print(position)
+		
+	if Input.is_action_just_pressed("ui_down") and currentLine.below != lineBaseReference.lowerBound:
+		currentLine = currentLine.below
+		position = currentLine.points[1]
+		print(position)
+		
